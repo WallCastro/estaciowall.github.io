f9bbc5879b7f6b4910a9bd8eb773c75adae15d3c
WallCastro <walmylson.castro@gmail.comm>
2021-10-25 21:25:48 -0300

    estaciowall.github.io.git

---
+++ 01-Primitive/primitive-types.js
@@ -0,0 +1,27 @@
+/**
+ * Int
+ * Flot
+ * Double
+ * Char
+ * Long
+ * Short
+ * Boolean
+ * Byte
+ */
+
+var numberA = 5;
+var numberB = 7;
+
+console.log(typeof(numberA));
+console.log(typeof(numberB));
+
+
+var operationA = numberA + numberB;
+var operationB = numberA / numberB;
+var operationC = numberA * numberB;
+var operationD = numberA - numberB;
+var operationE = numberA % numberB;
+
+console.log(operationA, operationB, operationC, operationD, operationE);
+
+
---
+++ 02-Strings/Strings.js
@@ -0,0 +1,9 @@
+var text = "O rato roeu a roupa do rei de roma";
+console.log(text.length);
+console.log(text.toLowerCase());
+console.log(text.toUpperCase());
+console.log(text.substring(0, 10));
+
+for (let index = 0; index < text.length; index++) {
+    console.log(text[index]);
+}
